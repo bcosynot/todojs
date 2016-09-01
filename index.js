bc7d46aa75951166c1b87d656d7cae7a1fe02fb9
Vivek Ranjan <vr3690@gmail.com>
2016-08-31 20:10:24 -0500

    Added: Templating using pug

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-
+app.set('view engine', 'pug');
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('hi',{'title': 'Hi!', 'message':'Hello World!'})
 });
 
 app.listen(3000, function () {
