4549be5fc042adb484e3789ee348d41fc221bdd6
Vivek Ranjan <vr3690@gmail.com>
2016-09-05 13:52:00 -0500

    Starting with the todo section

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 app.set('view engine', 'pug');
 app.get('/', function (req, res) {
-    res.render('hi',{'title': 'Hi!', 'message':'Hello World!'})
+    res.render('todo',{'title': 'TODO!'})
+});
+
+app.post('/add', function(req, res){
+    console.log("received:"+req.body.add);
+    res.render('add',{'add':req.body.add});
 });
 
 app.listen(3000, function () {
